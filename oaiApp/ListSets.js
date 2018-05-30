d617c9ec7d52c54d4df29eb6b792c8dfb8989abb
Corentin Laithier <laithier.corentin@gmail.com>
2018-05-30 10:23:09 +0200

    Move OaiApp to its own repository

--- oaiApp/ListSets.js
+++
@@ -1,8 +0,0 @@
-var xmlBase = require('./xmlBase.js');
-
-module.exports=function(host,res){
-  var xmldoc=xmlBase(null,null,host,'ListSets');
-  xmldoc+='<error code="noSetHierarchy">This repository does not support sets</error></OAI-PMH>';
-  res.set('Content-Type', 'application/xml');
-  res.send(xmldoc);
-}
